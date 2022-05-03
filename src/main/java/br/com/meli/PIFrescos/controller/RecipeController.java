5defa82bbd90ed05430bff7af371bcd81315fe07
Felipe Myose <felipe.myose@mercadolivre.com>
2022-05-03 15:08:20 -0300

    feat": add recipe controller put and delete endoints

--- src/main/java/br/com/meli/PIFrescos/controller/RecipeController.java
+++ src/main/java/br/com/meli/PIFrescos/controller/RecipeController.java
@@ -6,10 +6,14 @@ import br.com.meli.PIFrescos.service.interfaces.IRecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,5 +38,22 @@ public class RecipeController {
         recipeService.save(recipe);
 
         return new ResponseEntity<>(recipe, HttpStatus.CREATED);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Recipe> update(@RequestParam Integer recipeId,
+                                         @RequestBody RecipeForm recipeForm) {
+        Recipe recipeNewValues = RecipeForm.convert(recipeForm);
+
+        Recipe updatedRecipe = recipeService.update(recipeId, recipeNewValues);
+
+        return new ResponseEntity<>(updatedRecipe, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<String> delete(@PathVariable Integer recipeId) {
+        recipeService.removeById(recipeId);
+
+        return new ResponseEntity<>("Deleted", HttpStatus.ACCEPTED);
     }
 }
